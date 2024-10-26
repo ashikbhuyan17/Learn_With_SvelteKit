71ed68e806096f2f05609c392a1b224f01b2720e
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2024-10-27 00:11:02 +0600

    dynamic page add

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Welcome to Products page</h1>
+<p>product 1</p>
+<p>product 2</p>
+<p>product 3</p>
---
+++ src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {page} from '$app/stores'
+    const productId = $page.params.productId
+    console.log("🚀 ~ productId:", productId)
+</script>
+
+
+<p>dynamic page {productId} </p>
